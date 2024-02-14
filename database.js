02f8735ee29a045578f77db3f985fecbd2b6d51d
Chavozoom <isaquechaves10500@gmail.com>
2024-02-13 22:29:16 -0300

    updates

--- database.js
+++ database.js
@@ -11,61 +11,59 @@ const pool = new pg.Pool({
 
 pool.on('error', connect);
 
-const client = async function connect() {
+async function connect() {
     try {
-        const client = await pool.connect();
+        await pool.connect();
         console.log(`Connected to db ${URL}`);
-        return client;
     } catch (err) {
         console.error(`Error connecting to db: ${err}`);
         setTimeout(connect, 3000); // Retry connection after 3 seconds
     }
-};
+}
 
 connect();
 
 module.exports.insertTransaction = async function (account_id, amount, description, transaction_type) {
     try {
-        await client.query('BEGIN');
-
-        const updateBalanceQuery = `
-            WITH updated_balance AS (
-                UPDATE balances
-                SET amount = amount + $1
-                WHERE account_id = $2
-                RETURNING amount
-            ), inserted_transaction AS (
-                INSERT INTO transactions (account_id, amount, description, transaction_type)
-                VALUES ($2, $3, $4, $5)
-                RETURNING id
-            )
-            SELECT ub.amount AS saldo, a.limit_amount AS limite
-            FROM updated_balance ub
-            JOIN accounts a ON a.id = $2
-        `;
+        // Primeira query: Verifica se a transação é permitida
+        const { rows: [balance] } = await pool.query(`
+            SELECT b.amount, a.limit_amount
+            FROM balances b
+            JOIN accounts a ON b.account_id = a.id
+            WHERE b.account_id = $1
+        `, [account_id]);
 
         const adjustAmount = transaction_type === 'c' ? amount : -amount;
 
-        const { rows: [result] } = await client.query(updateBalanceQuery, [adjustAmount, account_id, amount, description, transaction_type]);
-
-        if (transaction_type === 'd' && result.saldo < -result.limite) {
+        if (transaction_type === 'd' && (balance.amount + adjustAmount) < -balance.limit_amount) {
             throw new Error('Saldo insuficiente para realizar a transação.');
         }
-        await client.query('COMMIT');
-        return result;
+
+        const result = await pool.query(`
+            WITH updated_balance AS (
+                UPDATE balances
+                SET amount = amount + $2
+                WHERE account_id = $1
+                RETURNING amount
+            ), inserted_transaction AS (
+                INSERT INTO transactions (account_id, amount, description, transaction_type)
+                VALUES ($1, $2, $3, $4)
+                RETURNING id
+            )
+            SELECT (SELECT amount FROM updated_balance) AS saldo, (SELECT limit_amount FROM accounts WHERE id = $1) AS limite;
+        `, [account_id, adjustAmount, description, transaction_type]);
+
+        return result.rows[0];
     } catch (err) {
-        await client.query('ROLLBACK');
         throw err;
-    } finally {
-        client.release();
     }
 };
 
 module.exports.getBalanceByAccountId = async function (account_id) {
     const query = `
         SELECT
-            a.limit_amount,
-            b.amount
+            a.limit_amount AS limite,
+            b.amount AS total 
         FROM accounts a
         JOIN balances b ON a.id = b.account_id
         WHERE a.id = $1;
@@ -77,9 +75,9 @@ module.exports.getBalanceByAccountId = async function (account_id) {
 module.exports.getTransactionsByAccountId = async function (account_id) {
     const query = `
         SELECT
-            amount AS valor,
-            transaction_type AS tipo,
-            description AS descricao,
+            amount,
+            transaction_type,
+            description,
             date AS realizada_em
         FROM transactions
         WHERE account_id = $1
